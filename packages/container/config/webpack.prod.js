aad863f0a1d1b28dc5a5dd2e88d8fe58caee68f4
MilitsaTsvetkova <37243078+MilitsaTsvetkova@users.noreply.github.com>
2021-10-22 13:12:58 +0300

    added public path

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,7 +8,8 @@ const domain = process.env.PRODUCTION_DOMAIN
 const prodConfig = {
     mode: 'production',
     output: {
-        filename: '[name].[contenthash].js'
+        filename: '[name].[contenthash].js',
+        publicPath: '/container/latest/'
     },
     plugins: [
         new ModuleFederationPlugin({
@@ -17,7 +18,7 @@ const prodConfig = {
                 marketing: `marketing@${domain}/marketing/remoteEntry.js`
             },
             shared: packageJson.dependencies
-        })
+        }),
     ]
 }
 
